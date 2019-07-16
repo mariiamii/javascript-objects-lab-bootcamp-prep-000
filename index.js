4ea9050385f7ad84cd963984b32477a582702ecf
Mariam Anuashvili <m.anuashvili@gmail.com>
2019-07-16 21:03:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var recipes = new Object({})
 var obj = {prop: 1} 
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  return Object.assign({},obj,{[key]:value})
+  return Object.assign({}, obj, {[key]: value})
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
@@ -11,17 +11,16 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   return object
 }
 
+var newObj = Object.assign({}, obj);
 function deleteFromObjectByKey(object, key) {
-  delete object.key;
-  return obj
+  delete newObj[key];
+  return newObj
 }
 
 function destructivelyDeleteFromObjectByKey(object, key) {
   delete object.key
   return object
 }
-
-
 
 
 
