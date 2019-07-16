f948435b64ba7862d970358ec9d97001b806708b
Mariam Anuashvili <m.anuashvili@gmail.com>
2019-07-16 21:04:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,18 +4,18 @@ var obj = {prop: 1}
 
 function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({}, obj, {[key]: value})
-}
+};
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key] = value
   return object
-}
+};
 
 var newObj = Object.assign({}, obj);
 function deleteFromObjectByKey(object, key) {
   delete newObj[key];
   return newObj
-}
+};
 
 function destructivelyDeleteFromObjectByKey(object, key) {
   delete object.key
